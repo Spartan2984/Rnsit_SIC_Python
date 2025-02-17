6be42637050701e3b8b7ca37518a373b644672af
sudhanshu2984 <sudhanshu2984@gmail.com>
2025-02-17 15:19:29 +0530

    Perfect square

--- day1/p2.py
+++ day1/p2.py
@@ -1,1 +1,9 @@
-print('Yashvardhan')
+from math import sqrt
+
+N = int(input())
+root = int(sqrt(N)) 
+
+if root * root == N:
+    print('N is a perfect square')
+else:
+    print('N is not a perfect square')
